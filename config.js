88a8d00ba477d32485ffc8b3f0e8c3762acc2c05
Brendimo-co <simanguliyev23@gmail.com>
2025-08-20 15:00:18 +0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// Replace with your Apps Script Web App URL (Deploy > New deployment > Web app)
+const GAS_ENDPOINT = "https://script.google.com/macros/s/AKfycbztbYAWNy4oYvwWL2VFJSAtdFFEpqMPVIH1zK-wt9sOvC8uqC2oOZDAcywrUVkbGRRYgg/exec";
+
+// Optional simple shared secret to block random posts (set the same on server)
+const SHARED_SECRET = "change_me_please";
