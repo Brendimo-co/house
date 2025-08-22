a0b047308dae9c27fe3288257b1c21f5a710cefa
Brendimo-co <simanguliyev23@gmail.com>
2025-08-22 14:11:34 +0400

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
  * SHARED_SECRET: same random string here and in Code.gs
  * USER_EMAIL: optional for now; later will be set from Google Sign-In
  */
-const GAS_ENDPOINT  = "https://script.google.com/macros/s/AKfycbzUPIE5jxc1bXjphA_L-e6HBK9l66Z-l0TLooBTa8Sd83dUba6UAfgh-mTMIzYUQQiE_Q/exec"; // <-- replace
+const GAS_ENDPOINT  = "https://script.google.com/macros/s/AKfycbwfNVT_qRDYswJCKFccH8P2Re6nRGxJ9fmvgyS8kM1RHIDaGAFfgx8-sYjA9GbBfSpWYQ/exec"; // <-- replace
 const SHARED_SECRET = "change_me_please"; // <-- replace with a long random string
 const USER_EMAIL    = "";                 // e.g., "pm@yourcompany.az" (optional)
 const CLIENT_VERSION = "cost-v2.2.0";
