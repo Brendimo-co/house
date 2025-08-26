bb5f97eac2eb4f877de5db0f66e413dcc0cf2f7e
Brendimo-co <simanguliyev23@gmail.com>
2025-08-26 16:52:38 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 // config.js
-const CONFIG = {
-  endpoint: 'https://script.google.com/macros/s/AKfycbzVBiEMBD_XhluiG0xPTRtG3R-Cg-6vqBdE_OdfVRX-2KoOWCPWVcHaCP5lA6A6SkZv8A/exec',  // e.g. https://script.google.com/macros/s/XXX/exec
-  sharedSecret: 'myStrongSecret123'
-};
+// Keep your CONFIG if you like, but also expose the globals that app.js uses.
+const GAS_ENDPOINT   = 'https://script.google.com/macros/s/AKfycbzVBiEMBD_XhluiG0xPTRtG3R-Cg-6vqBdE_OdfVRX-2KoOWCPWVcHaCP5lA6A6SkZv8A/exec';
+const SHARED_SECRET  = 'myStrongSecret123';
+
+// Optional (used by app.js if you want to display version/email)
+const USER_EMAIL     = '';         // fill if you want
+const CLIENT_VERSION = 'v1.0.0';
