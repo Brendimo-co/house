4b04e23b32eaae70b372dd1a8ce4c06442c3b6f6
Brendimo-co <simanguliyev23@gmail.com>
2025-08-26 16:56:25 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 // Keep your CONFIG if you like, but also expose the globals that app.js uses.
-const GAS_ENDPOINT   = 'https://script.google.com/macros/s/AKfycbzVBiEMBD_XhluiG0xPTRtG3R-Cg-6vqBdE_OdfVRX-2KoOWCPWVcHaCP5lA6A6SkZv8A/exec';
+const GAS_ENDPOINT   = 'https://script.google.com/macros/s/AKfycbw0oQoTqmlUrvFaI91l8V8mdcYna6zF6wenvaHpwDwebMUUUSwXBz4ErCziJiYnsPWW/exec';
 const SHARED_SECRET  = 'myStrongSecret123';
 
 // Optional (used by app.js if you want to display version/email)
